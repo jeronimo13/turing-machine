f5ab11ec20f3e45b49cb578e872eafadfada06bc
herman.havrysh <german@airshiphq.com>
2020-05-10 23:29:27 +0300

    Updates

---
+++ precache-manifest.1471633bf8c90574a148208f42ba229a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245dca9ad0d688aafd96b7797256074f",
+    "url": "/turing-machine/index.html"
+  },
+  {
+    "revision": "81fdcc4f55f8b40dfe33",
+    "url": "/turing-machine/static/css/main.2987c21e.chunk.css"
+  },
+  {
+    "revision": "cbe03399e98e8d7e4801",
+    "url": "/turing-machine/static/js/2.2c562286.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/turing-machine/static/js/2.2c562286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81fdcc4f55f8b40dfe33",
+    "url": "/turing-machine/static/js/main.d03938cb.chunk.js"
+  },
+  {
+    "revision": "dc6fa0991ee162283eae",
+    "url": "/turing-machine/static/js/runtime-main.3cdf5924.js"
+  }
+]);
